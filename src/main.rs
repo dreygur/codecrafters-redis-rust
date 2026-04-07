9bf7f86dbf0313e531942e323d81e3542baacb8a
Rakibul Yeasin <ryeasin03@gmail.com>
2026-04-07 14:28:23 +0600

    fix: make codecrafters scripts executable

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
