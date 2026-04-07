7248bfe7a0d0a1c68edf660903684dfbb9698c34
Rakibul Yeasin <ryeasin03@gmail.com>
2026-04-07 14:34:26 +0600

    fix: make codecrafters scripts executable

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-// Uncomment this block to pass the first stage
 use std::{
     io::{Read, Result, Write},
     net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
-                let _ = handle(&mut _stream);
+                thread::spawn(move || {
+                    let _ = handle(&mut stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -24,17 +23,12 @@ fn main() {
 }
 
 fn handle(stream: &mut TcpStream) -> Result<()> {
-    // stream.write(b"+PONG\r\n")?;
     let mut buffer = [0; 512];
     loop {
         match stream.read(&mut buffer) {
-            Ok(0) => {
-                println!("connection closed");
-                break;
-            }
-            Ok(n) => {
-                println!("read {} bytes", n);
-                stream.write(b"+PONG\r\n")?;
+            Ok(0) => break,
+            Ok(_) => {
+                stream.write_all(b"+PONG\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
